adac0850cd00213f0af2d3f21afe095c106daf98
aeiouxx <st55776@upce.cz>
2024-05-08 18:12:04 +0200

    lmao

--- src/runner.py
+++ src/runner.py
@@ -15,32 +15,31 @@ def main():
     seed = 1
     cities = matrix.parse_file(paths.input_file('cities.csv'), city_count)
     distances = matrix.create_distance_matrix(cities)
-    if not True:
-        print("-----------------HOPFIELD NETWORK------------------")
-        print(f"Running with seed {seed} for 2_000 iterations with {city_count} cities.")
-        network = HopfieldNetwork(distances.copy(), cities, seed=seed, log_level=logging.INFO)
-        result = network.run(2_000)
-        if result is not None:
-            decoded_route = [cities[i].name for i in result]
-            print(f"Solution found with seed {seed}.")
-            print(f"Solution distance: {calculate_distance(result, distances):.2f} km")
-            print(f"Solution tour: {decoded_route}")
-        else:
-            print(f"No solution found with seed {seed}.")
-    if not True:
-        print("-----------------ANT COLONY------------------------")
-        print("Running...")
-        ants = Ants(cities, distances.copy())
-        route, distance = ants.run()
-        if route is not None and distance is not None:
-            decoded_route = [cities[i].name for i in route]
-            print(f"Solution found.")
-            print(f"Solution distance: {distance:.2f} km.")
-            print(f"Solution tour: {decoded_route}")
-        else:
-            print("Ant Colony Optimization failed to find a route.")
-    print("-----------------GENETIC ALGORITHM-----------------")
+    print("-----------------HOPFIELD NETWORK------------------")
+    iterations = 375
+    print(f"Running with seed {seed} for {iterations} iterations with {city_count} cities...")
+    network = HopfieldNetwork(distances.copy(), cities, seed=seed, log_level=logging.DEBUG)
+    result = network.run(iterations)
+    if result is not None:
+        decoded_route = [cities[i].name for i in result]
+        print(f"Solution found with seed {seed}.")
+        print(f"Solution distance: {calculate_distance(result, distances):.2f} km")
+        print(f"Solution tour: {decoded_route}")
+    else:
+        print(f"No solution found with seed {seed}.")
+    print("-----------------ANT COLONY------------------------")
     print("Running...")
+    ants = Ants(cities, distances.copy())
+    route, distance = ants.run()
+    if route is not None and distance is not None:
+        decoded_route = [cities[i].name for i in route]
+        print(f"Solution found.")
+        print(f"Solution distance: {distance:.2f} km.")
+        print(f"Solution tour: {decoded_route}")
+    else:
+        print("Ant Colony Optimization failed to find a route.")
+    print("-----------------GENETIC ALGORITHM-----------------")
+    print(f"Running with seed {seed} with {city_count} cities...")
     genetak = Genetic(cities, distances.copy(), seed=seed)
     route, distance = genetak.run()
     if route is not None and distance is not None:
